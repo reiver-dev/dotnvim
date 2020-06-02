e79ed7dad8fe74ddd4b018f5c80ec25013caf0da
Andrey Bushev <hlam-box@yandex.ru>
2020-06-02 13:20:17 -0500

    Remove unused functions

--- lua/bootstrap/loaded.lua
+++ lua/bootstrap/loaded.lua
@@ -13,19 +13,4 @@ autocmd! SourcePost *\
 :call v:lua._register_loaded(expand('<afile>')) \
 "
 
-local M = {}
-
-
-function M.loadpackage(name)
-    vim.api.nvim_command("packadd " .. name)
-    _package_loaded[name] = true
-end
-
-
-function M.loadall(name)
-    vim.api.nvim_command("packloadall")
-end
-
-
-
 --- loaded.lua ends here
