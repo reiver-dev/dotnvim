afaac1522b951cb26a51cfc868887cfe24d51047
Andrey Bushev <hlam-box@yandex.ru>
2021-03-28 17:17:50 -0700

    Simplify fennel path resolve

--- lua/bootstrap/fennel/compiler.lua
+++ lua/bootstrap/fennel/compiler.lua
@@ -51,12 +51,37 @@ function M.compile_file(src, dst, opts, force)
 end
 
 
+local filesep, pathsep, submask
+do 
+    local it = string.gmatch(package.config, "([^\n]+)")
+    filesep = it() or "/"
+    pathsep = it() or ";"
+    submask = it() or "?"
+end
+local fnl_tail = string.format("%s.fnl", submask)
+local fnl_base = table.concat({
+    ".", filesep, submask, ".fnl", pathsep,
+    ".", filesep, submask, filesep, "init.fnl",
+})
+
+
+function M.resolve_path()
+    local paths = vim.api.nvim_get_runtime_file("fnl/", true)
+    local newpath = {fnl_base}
+    for i, path in ipairs(paths) do
+        newpath[#newpath + 1] = path .. fnl_tail
+    end
+    local aniseed_path = vim.api.nvim_get_runtime_file("lua/conjure/aniseed/macros.fnl", false)[1]
+    if aniseed_path then
+        newpath[#newpath + 1] = aniseed_path
+    end
+    return table.concat(newpath, ";")
+end
+
+
 function M.compiler_init()
     LOAD_PACKAGE("conjure")
-    local macros = vim.api.nvim_get_runtime_file("lua/conjure/aniseed/macros.fnl", false)[1]
-    local aniseed_root = vim.fn.fnamemodify(macros, ":h:h")
-    local fnl_root = vim.fn.stdpath("config") .. "/fnl"
-    fennel.path = ("%s/?.fnl;%s/?.fnl;%s"):format(aniseed_root, fnl_root, fennel.path)
+    fennel.path = M.resolve_path()
 end
 
 
