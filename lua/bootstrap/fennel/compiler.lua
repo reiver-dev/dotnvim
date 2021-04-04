4608416e10e255479c54a945c53db366136a06ce
Andrey Bushev <hlam-box@yandex.ru>
2021-04-04 16:11:01 -0700

    Use fennel macro-searchers

--- lua/bootstrap/fennel/compiler.lua
+++ lua/bootstrap/fennel/compiler.lua
@@ -4,6 +4,7 @@
 local M = {}
 
 local fennel = require "fennel"
+local basic = require "bootstrap.basic"
 
 function M.compile_module_source(text, opts)
     local code = "(require-macros \"aniseed.macros\")" .. text
@@ -18,76 +19,65 @@ function M.compile_source(text, opts)
 end
 
 
-local function slurp(path)
-    local fd, err = io.open(path, "r")
-    if fd == nil then
-        return false, err
+function M.compile_file(src, dst, opts)
+    local text = basic.slurp(src)
+    local compiled, code = M.compile_module_source(text, opts)
+    if compiled then
+        vim.fn.mkdir(vim.fn.fnamemodify(dst, ":h"), "p")
+        basic.spew(dst, code)
+    else
+        local msg = ("Failed compile: %s\n%s"):format(src, code)
+        vim.api.nvim_err_writeln(msg)
     end
-    local ok, data = pcall(function () return fd:read("*all") end)
-    fd:close()
-    return ok, data
 end
 
 
-local function spew(path, text)
-    local fd, err = io.open(path, "w")
-    if fd == nil then
-        return false, err
-    end
-    local ok, res = pcall(function() return fd:write(text) end)
-    fd:close()
-    return ok, res
+local function macro_loader(modname, fname)
+    return fennel.dofile(fname, {env = "_COMPILER"})
 end
 
 
-function M.compile_file(src, dst, opts, force)
-    if force ~= nil or vim.fn.getftime(src) > vim.fn.getftime(dst) then
-        local ok, text = slurp(src)
-        if ok then
-            local compiled, code = M.compile_module_source(text, opts)
-            if compiled then
-                vim.fn.mkdir(vim.fn.fnamemodify(dst, ":h"), "p")
-                spew(dst, code)
-            else
-                local msg = ("Failed compile: %s\n%s"):format(src, code)
-                vim.api.nvim_err_writeln(msg)
-            end
+local function macro_searcher(name)
+    local basename = string.gsub(name, "%.", "/")
+
+    local f = string.format
+    local paths = {
+        f("fnl/%s.fnl", basename),
+        f("fnl/%s/init.fnl", basename),
+    }
+
+    local get = vim.api.nvim_get_runtime_file
+
+    for _, path in ipairs(paths) do
+        local found = get(path, false)[1]
+        if found then
+            return macro_loader, found
         end
     end
+
+    return nil
 end
 
 
-local filesep, pathsep, submask
-do 
-    local it = string.gmatch(package.config, "([^\n]+)")
-    filesep = it() or "/"
-    pathsep = it() or ";"
-    submask = it() or "?"
-end
-local fnl_tail = string.format("%s.fnl", submask)
-local fnl_base = table.concat({
-    ".", filesep, submask, ".fnl", pathsep,
-    ".", filesep, submask, filesep, "init.fnl",
-})
-
-
-function M.resolve_path()
-    local paths = vim.api.nvim_get_runtime_file("fnl/", true)
-    local newpath = {fnl_base}
-    for i, path in ipairs(paths) do
-        newpath[#newpath + 1] = path .. fnl_tail
+local function aniseed_macro_searcher(name)
+    if name ~= "aniseed.macros" then
+        return nil
     end
-    local aniseed_path = vim.api.nvim_get_runtime_file("lua/conjure/aniseed/macros.fnl", false)[1]
-    if aniseed_path then
-        newpath[#newpath + 1] = aniseed_path
+    local path = vim.api.nvim_get_runtime_file("lua/conjure/aniseed/macros.fnl", false)[1]
+    if not path then
+        return nil
     end
-    return table.concat(newpath, ";")
+    return macro_loader, path
 end
 
 
 function M.compiler_init()
     LOAD_PACKAGE("conjure")
-    fennel.path = M.resolve_path()
+    local searchers = fennel["macro-searchers"]
+    if #searchers == 1 then
+        table.insert(searchers, 1, macro_searcher)
+        table.insert(searchers, aniseed_macro_searcher)
+    end
 end
 
 
