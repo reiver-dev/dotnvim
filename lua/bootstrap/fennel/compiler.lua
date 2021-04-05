04655083ffea3cb325836ec6c11d70b2b053ee18
Andrey Bushev <hlam-box@yandex.ru>
2021-04-04 19:11:30 -0700

    Force line-endings to unix

--- lua/bootstrap/fennel/compiler.lua
+++ lua/bootstrap/fennel/compiler.lua
@@ -20,7 +20,7 @@ end
 
 
 function M.compile_file(src, dst, opts)
-    local text = basic.slurp(src)
+    local text = basic.slurp(src):gsub("\r\n", "\n")
     local compiled, code = M.compile_module_source(text, opts)
     if compiled then
         vim.fn.mkdir(vim.fn.fnamemodify(dst, ":h"), "p")
@@ -71,10 +71,43 @@ local function aniseed_macro_searcher(name)
 end
 
 
+function M.resolve_path()
+    local filesep, pathsep, submask
+    do
+        local it = string.gmatch(package.config, "([^\n]+)")
+        filesep = it() or "/"
+        pathsep = it() or ";"
+        submask = it() or "?"
+    end
+
+    local fnl_tail = string.format("%s.fnl", submask)
+    local fnl_base = table.concat({
+        ".", filesep, submask, ".fnl", pathsep,
+        ".", filesep, submask, filesep, "init.fnl",
+    })
+
+    local newpath = {fnl_base}
+
+    local paths = vim.api.nvim_get_runtime_file("fnl/", true)
+    for i, path in ipairs(paths) do
+        newpath[#newpath + 1] = path .. fnl_tail
+    end
+
+    local aniseed_path = vim.api.nvim_get_runtime_file("lua/conjure/aniseed/macros.fnl", false)[1]
+    if aniseed_path then
+        newpath[#newpath + 1] = aniseed_path
+    end
+
+    return table.concat(newpath, ";")
+end
+
+
 function M.compiler_init()
     LOAD_PACKAGE("conjure")
     local searchers = fennel["macro-searchers"]
-    if #searchers == 1 then
+    if searchers == nil then
+        fennel.path = M.resolve_path()
+    elseif #searchers == 1 then
         table.insert(searchers, 1, macro_searcher)
         table.insert(searchers, aniseed_macro_searcher)
     end
