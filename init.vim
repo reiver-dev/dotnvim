ebb3bdf8ea08f0a622d0557be3b61c3ddc224eb7
Andrey Bushev <hlam-box@yandex.ru>
2021-09-07 10:48:46 -0700

    Simplify init, load after.lua as file instead of module

--- init.vim
+++ init.vim
@@ -1,82 +1,11 @@
 """ Main nvim config
 """
 
-function! s:clipboard_set(name)
-    return { lines, regtype -> v:lua.setclipboard(lines, regtype, name) }
-endfunction
+lua <<EOL
+    local mod = require("bootstrap")
+    mod.setup()
+    mod.setup_after()
+EOL
 
-
-function! s:clipboard_get(name)
-    return { -> v:lua.getclipboard(name) }
-endfunction
-
-
-function! ClipboardReset()
-    unlet! g:loaded_clipboard_provider
-    runtime autoload/provider/clipboard.vim
-endfunction
-
-
-function! ClipboardSetup()
-    let g:clipboard = {
-                \ 'name': 'bootstrap',
-                \     'copy': {
-                \         '+': call s:clipboard_set('+'),
-                \         '*': call s:clipboard_set('*'),
-                \     },
-                \     'paste': {
-                \         '+': call s:clipboard_get('+'),
-                \         '*': call s:clipboard_get('*'),
-                \     },
-                \ }
-endfunction
-
-
-function! s:has_element(obj, key)
-    return type(a:obj) == v:t_dict && has_key(a:obj, a:key) || 
-         \ type(a:obj) == v:t_list && 0 <= a:key && a:key < len(a:obj) 
-endfunction
-
-
-function! EvalMember(obj, path) abort
-    let pt = type(a:path)
-
-    if pt == v:t_dict
-        for [k, v] in items(a:path)
-	    if s:has_element(a:obj, k)
-                call EvalMember(a:obj[k], v)
-            endif
-        endfor
-    elseif pt == v:t_list
-	let k = 1
-        for v in a:path
-	    if s:has_element(a:obj, k)
-	        call EvalMember(a:obj[k], v)
-            endif
-	    let k += 1
-        endfor
-    else
-	if s:has_element(a:obj, a:path)
-            let a:obj[a:path] = eval(a:obj[a:path])
-        endif
-    endif
-endfunction
-
-
-function! Apply(target, onargs, args) abort
-    call EvalMember(a:args, a:onargs)
-    return call(function(a:target), a:args)
-endfunction
-
-
-function! ProjectRoot()
-    return v:lua._trampouline("my.project", "project-root")
-endfunction
-
-
-" lua require("bootstrap.main").setup()
-" lua require("bootstrap.main").runlisp()
-" lua require("bootstrap.main").finalize()
-lua require("bootstrap").setup()
 
 """ init.vim ends here
--- lua/bootstrap/init.lua
+++ lua/bootstrap/init.lua
@@ -5,6 +5,13 @@ local M = {}
 function M.setup()
     vim.o.termguicolors = true
 
+    vim.cmd [[
+        function! ClipboardReset()
+            unlet! g:loaded_clipboard_provider
+            runtime autoload/provider/clipboard.vim
+        endfunction
+    ]]
+
     if os.getenv("AK_PROFILER") then
         vim.cmd "packadd profiler"
         require("profiler")
@@ -19,29 +26,60 @@ function M.setup()
     require("bootstrap.gui").setup()
     require("my").setup()
 
-    -- Execute after module
-    local function init_package(package_name, ...)
-        local ok, res = xpcall(function()
-            return require(package_name)
-        end, debug.traceback)
+end
 
-        if ok then
-            if res then
-                if vim.is_callable(res) then
-                    return res(...)
-                elseif vim.is_callable(res.setup) then
-                    return res.setup(...)
+
+function M.setup_after()
+    local root = vim.fn.stdpath("config")
+    local name = "after.lua"
+    local path = root .. package.config:sub(1, 1) .. name
+    local after_source
+
+    do
+        local fd, err, errno = io.open(path, 'r')
+
+        if fd == nil then
+            if errno == 2 then
+                local fd, err = io.open(path, "w")
+                if fd then
+                    local t = "--- After config\n\n--- after.lua ends here\n"
+                    pcall(fd.write, fd, t)
+                    fd:close()
                 end
+            else
+                vim.notify(err:gsub("\t", "    "), vim.log.levels.ERROR)
             end
-        else
-            local errmsg = string.format("module '%s' not found", package_name)
-            if not vim.startswith(res, errmsg) then
-                vim.notify(res:gsub("\t", "    "), vim.log.levels.ERROR)
-            end
+            return
         end
+
+        local ok, text = pcall(fd.read, fd, "*all")
+        fd:close()
+
+        if ok ~= true then
+            return
+        end
+
+        after_source = text
     end
 
-    init_package("after")
+    local mod, err = loadstring(after_source, "@" .. path)
+    if mod == nil then
+        vim.notify(err:gsub("\t", "    "), vim.log.levels.ERROR)
+        return
+    end
+
+    local res = mod()
+
+    if type(res) == "function" then
+        return res()
+    elseif type(res) == "table" then
+        if vim.is_callable(res) then
+            return res()
+        elseif vim.is_callable(res.setup) then
+            return res.setup()
+        end
+    end
 end
+
 
 return M
