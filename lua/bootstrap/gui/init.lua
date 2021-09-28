4705c0de2bcd05f67b510e742dd683c94911381b
Andrey Bushev <abushev@fb.com>
2021-09-28 11:06:36 -0700

    Use hook instead of after module in gui config

--- lua/bootstrap/gui/init.lua
+++ lua/bootstrap/gui/init.lua
@@ -13,6 +13,21 @@ local M = {}
 
 
 M._pending_configuration = {}
+M._after_hook = {}
+
+
+local function execute_hook(hook, arg)
+    local errors = {}
+    for name, f in pairs(hook) do
+        local ok, msg = xpcall(function() return f(arg) end, debug.traceback)
+        if not ok then
+            errors[name] = msg
+        end
+    end
+    if log then
+        log("Failed to run after gui hook", "errors", errors)
+    end
+end
 
 
 local function client_info(chan)
@@ -59,21 +74,12 @@ function M.leave(chan)
 end
 
 
-function M.execute_after(info)
-    local ok, mod = pcall(function() return require"after.gui" end)
-    if ok and mod.setup then
-        mod.setup(info)
-    end
-end
-
-
 function M.configure()
     for _, chan in ipairs(M._pending_configuration) do
         local info = client_info(chan)
         if info then
-            if handle_gui_info(info) then
-                M.execute_after(info)
-            end
+            handle_gui_info(info)
+            execute_hook(M._after_hook, info)
         end
     end
     M._pending_configuration = {}
@@ -81,7 +87,12 @@ end
 
 
 function M.setup()
-   vim.api.nvim_exec(au, nil) 
+   vim.api.nvim_exec(au, nil)
+end
+
+
+function M.hook(name, func)
+    M._after_hook[name] = func
 end
 
 
