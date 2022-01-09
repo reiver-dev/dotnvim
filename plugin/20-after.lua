423adb26d92e5ff2ad03446bd1511af007f2ab40
Andrey Bushev <hlam-box@yandex.ru>
2022-01-08 22:25:31 -0800

    Unwrap function in 'after'

--- plugin/20-after.lua
+++ plugin/20-after.lua
@@ -1,51 +1,49 @@
-(function()
-    local root = vim.fn.stdpath("config")
-    local name = "after.lua"
-    local path = root .. package.config:sub(1, 1) .. name
-    local after_source
+local root = vim.fn.stdpath("config")
+local name = "after.lua"
+local path = root .. package.config:sub(1, 1) .. name
+local after_source
 
-    do
-        local fd, err, errno = io.open(path, 'r')
+do
+    local fd, err, errno = io.open(path, 'r')
 
-        if fd == nil then
-            if errno == 2 then
-                local fd = io.open(path, "w")
-                if fd then
-                    local t = "--- After config\n\n--- after.lua ends here\n"
-                    pcall(fd.write, fd, t)
-                    fd:close()
-                end
-            else
-                vim.notify(err:gsub("\t", "    "), vim.log.levels.ERROR)
+    if fd == nil then
+        if errno == 2 then
+            local fd = io.open(path, "w")
+            if fd then
+                local t = "--- After config\n\n--- after.lua ends here\n"
+                pcall(fd.write, fd, t)
+                fd:close()
             end
-            return
+        else
+            vim.notify(err:gsub("\t", "    "), vim.log.levels.ERROR)
         end
-
-        local ok, text = pcall(fd.read, fd, "*all")
-        fd:close()
-
-        if ok ~= true then
-            return
-        end
-
-        after_source = text
-    end
-
-    local mod, err = loadstring(after_source, "@" .. path)
-    if mod == nil then
-        vim.notify(err:gsub("\t", "    "), vim.log.levels.ERROR)
         return
     end
 
-    local res = mod()
+    local ok, text = pcall(fd.read, fd, "*all")
+    fd:close()
 
-    if type(res) == "function" then
-        return res()
-    elseif type(res) == "table" then
-        if vim.is_callable(res) then
-            return res()
-        elseif vim.is_callable(res.setup) then
-            return res.setup()
-        end
+    if ok ~= true then
+        return
     end
-end)()
+
+    after_source = text
+end
+
+local mod, err = loadstring(after_source, "@" .. path)
+if mod == nil then
+    vim.notify(err:gsub("\t", "    "), vim.log.levels.ERROR)
+    return
+end
+
+local res = mod()
+
+if type(res) == "function" then
+    res()
+elseif type(res) == "table" then
+    if vim.is_callable(res) then
+        res()
+    elseif vim.is_callable(res.setup) then
+        res.setup()
+    end
+end
