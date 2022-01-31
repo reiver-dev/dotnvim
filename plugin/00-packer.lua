7cd2b515a7e23624fb70066e55c21250fd44ae61
Andrey Bushev <hlam-box@yandex.ru>
2022-01-31 09:20:03 -0800

    Push packer loader after preload and fennel_cache loader

--- plugin/00-packer.lua
+++ plugin/00-packer.lua
@@ -1,9 +1,16 @@
 do
     local sep = package.config:sub(1, 1)
     local path = vim.fn.stdpath("data") .. sep .. "packer_compiled.lua"
-    local mod, err = loadfile(path)
-    if mod then
-        mod()
+    local packer_compiled, err = loadfile(path)
+    if packer_compiled then
+        packer_compiled()
+        -- pull preload and fennel loaders before
+        -- packer's lazy loader
+        local loaders = package.loaders
+        local lazy_loader = loaders[1]
+        loaders[1] = loaders[2]
+        loaders[2] = loaders[3]
+        loaders[3] = lazy_loader
     elseif not err:match("No such file or directory$") then
         error(err)
     end
