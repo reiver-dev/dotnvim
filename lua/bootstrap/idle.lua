085bec0aabea2e82993cada776f388467f91cd1c
Andrey Bushev <hlam-box@yandex.ru>
2022-05-23 00:15:49 -0700

    Idle callbak to use lua autocmd api

--- lua/bootstrap/idle.lua
+++ lua/bootstrap/idle.lua
@@ -1,6 +1,4 @@
-local exec = vim.cmd
-
-local eventignore = vim.opt.eventignore
+--- Idle timer detached from CursorHold'I event
 
 local timer = vim.loop.new_timer()
 local timeout = 250
@@ -31,28 +29,37 @@ local function call_hook(is_insert)
 end
 
 
+local forward_autocmd
+do
+    local eventignore = vim.opt.eventignore
+    local nvim_exec_autocmds = vim.api.nvim_exec_autocmds
+    local opts = { modeline = false }
+    forward_autocmd = function(name)
+        eventignore:remove(name)
+        nvim_exec_autocmds(name, opts)
+        eventignore:append(name)
+    end
+end
+
+
 local on_timer_normal = vim.schedule_wrap(function()
     pcall(call_hook, false)
-    eventignore:remove "CursorHold"
-    exec "doautocmd <nomodeline> CursorHold"
-    eventignore:append "CursorHold"
+    forward_autocmd("CursorHold")
 end)
 
 
 local on_timer_insert = vim.schedule_wrap(function()
     pcall(call_hook, true)
-    eventignore:remove "CursorHoldI"
-    exec "doautocmd <nomodeline> CursorHoldI"
-    eventignore:append "CursorHoldI"
+    forward_autocmd("CursorHoldI")
 end)
 
 
 local not_recording
 do
     local empty = {}
-    local nvim_call_function = vim.api.nvim_call_function
+    local call = vim.call
     not_recording = function()
-        return nvim_call_function("reg_recording", empty) == ""
+        return call("reg_recording", empty) == ""
     end
 end
 
@@ -74,15 +81,6 @@ local function on_idle_break(is_insert)
         timer:start(timeout, 0, cb)
     end
 end
-
-
-local COMMAND = [[
-    augroup idle_timer
-        autocmd!
-        autocmd CursorMoved * lua __on_idle_break(false)
-        autocmd CursorMovedI * lua __on_idle_break(true)
-    augroup END
-]]
 
 
 --- Add idle hook
@@ -107,9 +105,25 @@ end
 
 --- Initialize idle hook and CursorHold'I passing
 local function setup()
-    eventignore:append({ "CursorHold", "CursorHoldI" })
-    exec(COMMAND)
-    _G.__on_idle_break = on_idle_break
+    vim.opt.eventignore:append({ "CursorHold", "CursorHoldI" })
+
+    local ag = vim.api.nvim_create_augroup("idle_timer", { clear = true, })
+
+    vim.api.nvim_create_autocmd("CursorHold", {
+        group = ag,
+        desc = "bootstrap.idle::on_idle_break(is_insert: false)",
+        callback = function()
+            on_idle_break(false)
+        end
+    })
+
+    vim.api.nvim_create_autocmd("CursorHoldI", {
+        group = ag,
+        desc = "bootstrap.idle::on_idle_break(is_insert: true)",
+        callback = function()
+            on_idle_break(true)
+        end
+    })
 end
 
 
